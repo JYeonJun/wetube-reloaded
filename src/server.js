5ec4a40e666281741e8ab77066c0fab8923b17be
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-02-21 17:51:01 +0900

    get request

--- src/server.js
+++ src/server.js
@@ -5,8 +5,11 @@ const PORT = 4000;
 // express application 생성
 const app = express();
 
+// get request
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 // 외부 접속 listen
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🤖`);
 
-app.listen(4000, handleListening); // port번호, callback함수
+app.listen(PORT, handleListening); // port번호, callback함수
