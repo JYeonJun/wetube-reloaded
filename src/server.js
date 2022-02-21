4981dd450bc432dc92a4a31f39ce3528a078595f
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-02-21 17:14:39 +0900

    Updated server.js
    주석 추가

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const PORT = 4000;
 
+// express application 생성
 const app = express();
 
+// 외부 접속 listen
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🤖`);
 
