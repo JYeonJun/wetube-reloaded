c82c50bdf06949440f866423d5e48024e9fda387
JYeonJun <jyj3898@naver.com>
2022-02-22 15:56:20 +0900

    Add middleware

--- src/server.js
+++ src/server.js
@@ -5,8 +5,14 @@ const PORT = 4000;
 // express application 생성
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  next();
+};
+
 // get request
-app.get("/", (req, res) => res.send("<h1>I still love you<h1>"));
+app.get("/", gossipMiddleware, (req, res, next) =>
+  res.send("<h1>I still love you<h1>")
+);
 app.get("/login", (req, res) => res.send("Login here."));
 
 // 외부 접속 listen
