a5dafb68318824447aa024484a79b5408c6cf842
JYeonJun <jyj3898@naver.com>
2022-02-22 17:03:00 +0900

    Study middleware

--- src/server.js
+++ src/server.js
@@ -5,18 +5,27 @@ const PORT = 4000;
 // express application 생성
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
   next();
 };
 
 // get request
-app.get("/", gossipMiddleware, (req, res, next) =>
-  res.send("<h1>I still love you<h1>")
-);
-app.get("/login", (req, res) => res.send("Login here."));
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", (req, res) => res.send("<h1>I still love you<h1>"));
+app.get("/protected", (req, res) => res.send("Welcome to the private lounge."));
 
 // 외부 접속 listen
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🤖`);
+  console.log(`Server listening osn port http://localhost:${PORT} 🤖`);
 
 app.listen(PORT, handleListening); // port번호, callback함수
