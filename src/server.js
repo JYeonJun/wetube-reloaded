110c0f94fa39a71a69caab4f3de16e46cfccf859
JYeonJun <jyj3898@naver.com>
2022-02-23 18:20:11 +0900

    Cleaning the Code#1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
@@ -12,31 +15,12 @@ const logger = morgan("dev");
 // get request
 app.use(logger);
 
-// 라우터 생성
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 // 외부 접속 listen
 const handleListening = () =>
-  console.log(`Server listening osn port http://localhost:${PORT} 🤖`);
+  console.log(`Server listening on port http://localhost:${PORT} 🤖`);
 
 app.listen(PORT, handleListening); // port번호, callback함수
