9d4d46185f1029dee5b47e7860725abe6fed6d3a
JYeonJun <jyj3898@naver.com>
2022-02-23 16:25:32 +0900

    Download Morgan

--- src/server.js
+++ src/server.js
@@ -1,28 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 // express application 생성
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  next();
-};
+const logger = morgan("dev");
 
 // get request
 app.use(logger);
-app.use(privateMiddleware);
 app.get("/", (req, res) => res.send("<h1>I still love you<h1>"));
-app.get("/protected", (req, res) => res.send("Welcome to the private lounge."));
 
 // 외부 접속 listen
 const handleListening = () =>
