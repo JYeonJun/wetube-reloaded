576d9513d84e44e272068033a14dfc7b24c15c82
JYeonJun <jyj3898@naver.com>
2022-02-23 17:43:56 +0900

    Created Routers

--- src/server.js
+++ src/server.js
@@ -6,11 +6,34 @@ const PORT = 4000;
 // express application 생성
 const app = express();
 
+// morgan으로 middleware 생성
 const logger = morgan("dev");
 
 // get request
 app.use(logger);
-app.get("/", (req, res) => res.send("<h1>I still love you<h1>"));
+
+// 라우터 생성
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 // 외부 접속 listen
 const handleListening = () =>
